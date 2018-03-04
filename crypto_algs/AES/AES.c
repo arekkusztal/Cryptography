252b5f55d6c0d7c86572d0e59ff813c7f27b937d
Arek Kusztal <arekkuszal@gmail.com>
2018-03-04 09:26:51 +0000

    AES: add decryption
    Signed-off-by: Arek Kusztal <arekkuszal@gmail.com>

--- crypto_algs/AES/AES.c
+++ crypto_algs/AES/AES.c
@@ -161,8 +161,28 @@ uint8_t s_box[256] =
    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
 };
 
+unsigned char s_box_inv[256] =
+{
+   0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+   0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+   0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+   0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+   0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+   0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+   0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+   0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+   0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+   0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+   0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+   0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+   0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+   0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+   0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+   0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D
+};
 
-uint8_t expansion[14][32];
+#define hex_dump_2_(a,b,c,d) hex_dump(a,b,c,d)
+#define hex_dump_2(a,b,c,d)
 
 void rol(uint8_t *data, uint16_t bg)
 {
@@ -218,6 +238,21 @@ void mix_columns(uint8_t *state)
     	state[i+4] = temp[i] ^ GMUL2[temp[i + 4]] ^ GMUL3[temp[i+8]] ^ temp[i+12];
     	state[i+8] = temp[i] ^ temp[i + 4] ^ GMUL2[temp[i+8]] ^ GMUL3[temp[i+12]];
     	state[i+12] = GMUL3[temp[i]] ^ temp[i + 4] ^ temp[i+8] ^ GMUL2[temp[i+12]];
+    }
+}
+
+void reverse_mix_columns(uint8_t *state)
+{
+	int i;
+	uint8_t temp[16];
+
+	memcpy(temp, state, 16);
+
+    for (i = 0; i < 4; i+=1) {
+    	state[i] = GMUL14[temp[i]] ^ GMUL11[temp[i+4]] ^ GMUL13[temp[i+8]] ^ GMUL9[temp[i+12]];
+    	state[i+4] = GMUL9[temp[i]] ^ GMUL14[temp[i + 4]] ^ GMUL11[temp[i+8]] ^ GMUL13[temp[i+12]];
+    	state[i+8] = GMUL13[temp[i]] ^ GMUL9[temp[i + 4]] ^ GMUL14[temp[i+8]] ^ GMUL11[temp[i+12]];
+    	state[i+12] = GMUL11[temp[i]] ^ GMUL13[temp[i + 4]] ^ GMUL9[temp[i+8]] ^ GMUL14[temp[i+12]];
     }
 }
 
@@ -354,17 +389,49 @@ AES_expand_keys(struct AES_context *ctx)
 	return ctx->expansion;
 }
 
+void AES_decrypt(uint8_t *out, uint8_t *in, uint8_t *key_2,
+				enum operation op)
+{
+
+	int i;
+	uint8_t state[16], key_inv[16], key_inv_2[16];
+
+	matrix_inverse(key_inv_2, key_2);
+	matrix_inverse(state, out);
+
+	for (i = 0 ;i < 16; i++) {
+		state[i] = state[i] ^ key_inv_2[i];
+	}
+
+	if (op != LAST)
+		reverse_mix_columns(state);
+
+	for (i = 1; i < RIJNDAEL; i++) {
+		ror(&state[RIJNDAEL*i], i);
+	}
+
+	for (i = 0; i<16; i++) {
+		state[i] = s_box_inv[state[i]];
+	}
+
+	if (op == FIRST) {
+		matrix_inverse(key_inv, in);
+		for (i = 0; i < 16; i++) {
+			state[i] ^= key_inv[i];
+		}
+	}
+
+	matrix_inverse(out, state);
+}
+
+
 void AES_encrypt(uint8_t *out, uint8_t *in, uint8_t *key_2,
 				enum operation op)
 {
 	int i;
 	uint8_t state[16], key_inv[16], key_inv_2[16];
 
-	printf("\n ------------- ENCRYPTION STEPS --------------");
-
-	hex_dump("out", out, 16, 16);
 	matrix_inverse(state, out);
-	hex_dump("matrix_inverse: state", state, 16, 16);
 	matrix_inverse(key_inv_2, key_2);
 
 	if (op == FIRST) {
@@ -373,20 +440,15 @@ void AES_encrypt(uint8_t *out, uint8_t *in, uint8_t *key_2,
 			state[i] ^= key_inv[i];
 		}
 
-		hex_dump("first xor: state", state, 16, 16);
 	}
 
 	for (i = 0; i<16; i++) {
 		state[i] = s_box[state[i]];
 	}
 
-	hex_dump("after s_box: state", state, 16, 16);
-
 	for (i = 1; i < RIJNDAEL; i++) {
 		rol(&state[RIJNDAEL*i], i);
 	}
-
-	hex_dump("after 3 rol: state", state, 16, 16);
 
 	if (op != LAST) {
 		mix_columns(state);
@@ -396,47 +458,32 @@ void AES_encrypt(uint8_t *out, uint8_t *in, uint8_t *key_2,
 		state[i] = state[i] ^ key_inv_2[i];
 	}
 
-	hex_dump("after OTP: state", state, 16, 16);
-
 	matrix_inverse(out, state);
+}
 
-	hex_dump("last inverse: state", out, 16, 16);
-
-
-	printf("\n ------------!- ENCRYPTION STEPS BACKWARDS -!-------------");
-
-	matrix_inverse(state, out);
-
-	hex_dump("REVERSE: last inverse: state", state, 16, 16);
-
-	for (i = 0 ;i < 16; i++) {
-		state[i] = state[i] ^ key_inv_2[i];
+void AES_decrypt_block(uint8_t *plaintext, struct CRYPTO_context *ctx)
+{
+	int i;
+	AES_decrypt(plaintext, NULL, &ctx->expansion[AES_BLOCK_SZ * ctx->key_rounds], LAST);
+	for (i = ctx->key_rounds - 1; i >= 2; i--) {
+		AES_decrypt(plaintext, NULL, &ctx->expansion[AES_BLOCK_SZ * i], STD);
 	}
-
-	hex_dump("REVERSE: after OTP: state", state, 16, 16);
-
-	hex_dump("REVERSE: after rijndeal mix: state", state, 16, 16);
-
-
-	for (i = 1; i < RIJNDAEL; i++) {
-		ror(&state[RIJNDAEL*i], i);
-	}
-
-	hex_dump("REVERSE: after 3 ror: state", state, 16, 16);
-
-	printf("\n ------------------------ END ----------------------------");
+	AES_decrypt(plaintext, ctx->key, ctx->expansion + AES_BLOCK_SZ, FIRST);
 }
 
 void AES_encrypt_block(uint8_t *plaintext, struct CRYPTO_context *ctx)
 {
 	int i;
+
 	AES_encrypt(plaintext, ctx->key, ctx->expansion + AES_BLOCK_SZ, FIRST);
 
-/*	for (i = 2; i <= ctx->key_rounds - 1; i++) {
+	for (i = 2; i <= ctx->key_rounds - 1; i++) {
 		AES_encrypt(plaintext, NULL, &ctx->expansion[AES_BLOCK_SZ * i], STD);
 	}
 
-	AES_encrypt(plaintext, NULL, &ctx->expansion[AES_BLOCK_SZ * ctx->key_rounds], LAST); */
+	AES_encrypt(plaintext, NULL, &ctx->expansion[AES_BLOCK_SZ * ctx->key_rounds], LAST);
+
+
 }
 
 /* TO BE MOVED */
